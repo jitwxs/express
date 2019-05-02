cfd9f95a941cd1422ddd99ff760f7f5a64a48f3f
jitwxs <jitwxs@foxmail.com>
2019-05-02 17:30:09 +0800

    管理员个人中心 100%

--- src/main/resources/static/assets/js/express.js
+++ src/main/resources/static/assets/js/express.js
@@ -6,3 +6,40 @@ function isPoneAvailable(tel) {
         return true;
     }
 }
+
+//添加cookie
+function addCookie(name, value, expiresHours) {
+    let cookieString = name + "=" + escape(value);
+    //判断是否设置过期时间,0代表关闭浏览器时失效
+    if (expiresHours > 0) {
+        let date = new Date();
+        date.setTime(date.getTime() + expiresHours * 1000);
+        cookieString = cookieString + ";expires=" + date.toUTCString();
+    }
+    document.cookie = cookieString;
+}
+
+//修改cookie的值
+function editCookie(name, value, expiresHours) {
+    let cookieString = name + "=" + escape(value);
+    if (expiresHours > 0) {
+        let date = new Date();
+        date.setTime(date.getTime() + expiresHours * 1000); //单位是毫秒
+        cookieString = cookieString + ";expires=" + date.toGMTString();
+    }
+    document.cookie = cookieString;
+}
+
+//根据名字获取cookie的值
+function getCookieValue(name) {
+    let strCookie = document.cookie;
+    let arrCookie = strCookie.split("; ");
+    for (let i = 0; i < arrCookie.length; i++) {
+        let arr = arrCookie[i].split("=");
+        if (arr[0] === name) {
+            return unescape(arr[1]);
+        } else {
+            return "";
+        }
+    }
+}
